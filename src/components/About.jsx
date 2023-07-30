9d99255b982ef33075a0642bc6f90a17a636bd51
Sarthak Gupta <gsarthak783891@gmail.com>
2023-07-30 14:46:03 +0530

    cards done on home

---
+++ src/components/About.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { FiChevronRight } from 'react-icons/fi'
+import { Link } from 'react-router-dom'
+
+function About() {
+  return (
+    <section className='flex flex-col md:flex-row p-4 gap-8 max-w-[1080px]  h-min w-full mt-20 items-center'>
+      <div className='flex flex-col items-center justify-center flex-1 gap-4 text-center md:items-start md:text-left'>
+        <span className='text-lg font-semibold text-primaryGreen'>
+          About Us
+        </span>
+        <span className='text-3xl font-bold md:text-5xl text-tealGreen'>
+          Renewable Energy for a Sustainable World
+        </span>
+        <span className='text-lg'>
+          Empowering businesses since 2016. Maximum savings on gas and
+          electricity. Unbeatable deals from top UK suppliers. No-obligation
+          service. Unique energy reduction solutions. Join us for cost-effective
+          energy management.
+        </span>
+        <Link className='flex items-center gap-1'>
+          Read More About Us <FiChevronRight />
+        </Link>
+      </div>
+      <div className='flex-1'>
+        <img
+          src='./aboutus.jpg'
+          alt=''
+          className='rounded-md'
+        />
+      </div>
+    </section>
+  )
+}
+
+export default About
--- src/components/screens/Home.jsx
+++ src/components/screens/Home.jsx
@@ -3,6 +3,7 @@ import Navbar from '../Navbar'
 import { Link } from 'react-router-dom'
 import { FiChevronRight, FiClock, FiPhoneCall } from 'react-icons/fi'
 import { FaVoicemail } from 'react-icons/fa'
+import About from '../About'
 
 const Home = () => {
   return (
@@ -25,7 +26,7 @@ const Home = () => {
             your Utilities bill.
           </div>
           <div className='flex gap-2 md:gap-4'>
-            <Link className='px-6 py-2 transition-all border rounded-full md:border-2 border-primaryGreen font-m6 dium md:py-6 h-max md:px-12 bg-primaryGreen hover:text-tealGreen md:text-xl'>
+            <Link className='px-6 py-2 transition-all border rounded-full md:border-2 border-primaryGreen font-m6 md:py-6 h-max md:px-12 bg-primaryGreen hover:text-tealGreen md:text-xl'>
               Contact Us
             </Link>
             <Link className='px-6 py-2 transition-all border rounded-full md:border-2 font-m6 dium md:py-6 h-max md:px-12 hover:bg-white/50 hover:text-tealGreen md:text-xl'>
@@ -38,7 +39,7 @@ const Home = () => {
       <section className='flex flex-col md:flex-row px-8 gap-4 md:gap-8 max-w-[1440px] -mt-16'>
         <div className='bg-primaryGreen shadow-xl rounded-xl p-8 flex flex-col items-center text-center gap-2 min-w-[300px] justify-center'>
           <span className='text-3xl font-bold text-white'>€OO'S</span>
-          <span className='text-xl font-medium'>
+          <span className='text-xl font-medium text-slate-200'>
             Savings Is Just One Click Away.
           </span>
         </div>
@@ -63,90 +64,66 @@ const Home = () => {
         </div>
       </section>
       {/* About Us */}
-      <section className='flex flex-col md:flex-row p-4 gap-8 max-w-[1080px] h-min w-full mt-20 items-center'>
-        <div className='flex flex-col items-center justify-center flex-1 gap-4 text-center md:items-start md:text-left'>
-          <span className='text-lg font-semibold text-primaryGreen'>
-            About Us
+      <About />
+      {/* 3 cards */}
+      <section className='flex flex-col items-center w-full gap-16 py-10 mt-20 text-white bg-tealGreen'>
+        <div className='text-4xl font-bold'>Services Provides By Us</div>
+        <div className='flex flex-col gap-6 py-10 md:flex-row text-tealGreen'></div>
+      </section>
+      {/* cards */}
+      <section className='flex flex-col gap-10 -mt-20 md:flex-row text-tealGreen'>
+        {/* card 1 */}
+        <div className='flex flex-col items-center justify-center w-auto gap-6 pb-6 overflow-hidden text-center bg-white shadow-xl rounded-xl'>
+          <img
+            src='./electricity.jpg'
+            alt=' '
+            className='object-cover brightness-90 w-[450px] h-80'
+          />
+
+          <span className='text-2xl font-bold text-tealGreen'>Electricity</span>
+          <span className='px-4 font-medium w-96 text-l text-slate-500'>
+            Since the deregulation in the energy market, many changes have taken
+            place with the most significant one is the price increases.
           </span>
-          <span className='text-3xl font-bold md:text-5xl text-tealGreen'>
-            Renewable Energy for a Sustainable World
-          </span>
-          <span className='text-lg'>
-            Empowering businesses since 2016. Maximum savings on gas and
-            electricity. Unbeatable deals from top UK suppliers. No-obligation
-            service. Unique energy reduction solutions. Join us for
-            cost-effective energy management.
-          </span>
-          <Link className='flex items-center gap-1'>
-            Read More About Us <FiChevronRight />
+          <Link className='px-12 py-4 font-medium text-white transition-all rounded-full bg-primaryGreen hover:text-tealGreen'>
+            Read More About Electricity
           </Link>
         </div>
-        <div className='flex-1'>
+
+        {/* card 2 */}
+        <div className='flex flex-col items-center justify-center w-auto gap-6 pb-6 overflow-hidden text-center bg-white shadow-xl rounded-xl'>
           <img
-            src='./aboutus.jpg'
-            alt=''
-            className='rounded-md'
+            src='./gas.jpg'
+            alt=' '
+            className='object-cover brightness-90 w-[450px] h-80 '
           />
+
+          <span className='text-2xl font-bold text-tealGreen'>Gas</span>
+          <span className='px-4 font-medium w-96 text-l text-slate-500'>
+            Since the deregulation in the energy market, many changes have taken
+            place with the most significant one is the price increases.
+          </span>
+          <Link className='px-12 py-4 font-medium text-white transition-all rounded-full bg-primaryGreen hover:text-tealGreen'>
+            Read More About Gas
+          </Link>
         </div>
-      </section>
-      {/* 3 cards */}
-      <section className='flex flex-col items-center w-full gap-16 p-4 py-20 mt-20 text-white bg-tealGreen'>
-        <div className='text-4xl font-bold'>Services Provides By Us</div>
-        <div className='flex flex-col gap-6 md:flex-row text-tealGreen'>
-          <div className='flex flex-col items-center justify-center gap-6 pb-6 overflow-hidden text-center bg-white rounded-xl w-96'>
-            <img
-              src='./electricity.jpg'
-              alt=' '
-              className='object-cover w-96 h-60 '
-            />
 
-            <span className='text-2xl font-bold text-tealGreen'>
-              Electricity
-            </span>
-            <span className='px-4 font-medium text-l text-slate-500'>
-              Since the deregulation in the energy market, many changes have
-              taken place with the most significant one is the price increases.
-            </span>
-            <Link className='px-12 py-4 font-medium transition-all rounded-full bg-primaryGreen hover:text-tealGreen'>
-              Read More About Electricity
-            </Link>
-          </div>
-          <div className='flex flex-col items-center justify-center gap-6 pb-6 overflow-hidden text-center bg-white rounded-xl w-96'>
-            <img
-              src='./electricity.jpg'
-              alt=' '
-              className='object-cover w-96 h-60 '
-            />
+        {/* card 3 */}
+        <div className='flex flex-col items-center justify-center w-auto gap-6 pb-6 overflow-hidden text-center bg-white shadow-xl rounded-xl'>
+          <img
+            src='./water.jpg'
+            alt=' '
+            className='object-cover w-[450px] brightness-90 h-80 '
+          />
 
-            <span className='text-2xl font-bold text-tealGreen'>
-              Electricity
-            </span>
-            <span className='px-4 font-medium text-l text-slate-500'>
-              Since the deregulation in the energy market, many changes have
-              taken place with the most significant one is the price increases.
-            </span>
-            <Link className='px-12 py-4 font-medium transition-all rounded-full bg-primaryGreen hover:text-tealGreen'>
-              Read More About Electricity
-            </Link>
-          </div>
-          <div className='flex flex-col items-center justify-center gap-6 pb-6 overflow-hidden text-center bg-white rounded-xl w-96'>
-            <img
-              src='./electricity.jpg'
-              alt=' '
-              className='object-cover w-96 h-60 '
-            />
-
-            <span className='text-2xl font-bold text-tealGreen'>
-              Electricity
-            </span>
-            <span className='px-4 font-medium text-l text-slate-500'>
-              Since the deregulation in the energy market, many changes have
-              taken place with the most significant one is the price increases.
-            </span>
-            <Link className='px-12 py-4 font-medium transition-all rounded-full bg-primaryGreen hover:text-tealGreen'>
-              Read More About Electricity
-            </Link>
-          </div>
+          <span className='text-2xl font-bold text-tealGreen'>Water</span>
+          <span className='px-4 font-medium text-l w-96 text-slate-500'>
+            From April 2017, the laws changed allowing business & charities to
+            seek better deals for water.
+          </span>
+          <Link className='px-12 py-4 font-medium transition-all rounded-full bg-primaryGreen hover:text-tealGreen'>
+            Read More About Water
+          </Link>
         </div>
       </section>
     </main>
