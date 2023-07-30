1b3891fdb9702f278fc9c97543475fdf830c7615
swaraj Saxena <swarishraj00@gmail.com>
2023-07-30 22:47:41 +0530

    footer done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,56 +1,95 @@
 import React from 'react'
+import { FiChevronRight } from 'react-icons/fi'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
-    <div className='flex justify-center px-6 py-12 lg:px-10 lg:py-20 bg-primaryGreen text-teal-950'>
-      <div className='max-w-[1224px] w-full flex flex-col lg:flex-row justify-between gap-4 lg:gap-2 '>
-        <div className='flex flex-col items-start gap-4 max-w-[400px]'>
-          <img src='./logo.png' />
-          <div className=''>
-            Here at utilities save. our aim is to provide you with maximum
-            savings and best services for your business utilities. we search
-            across most of the uk suppliers both small and large and identify
-            what is the best possible deal for your business.
+    <>
+      <div className='flex justify-center px-6 py-12 lg:px-10 lg:py-20 bg-lightGreen text-tealGreen'>
+        <div className='max-w-[1440px] w-full flex flex-col lg:flex-row justify-between gap-4 lg:gap-2 '>
+          <div className='flex flex-col items-start gap-4 max-w-[400px]'>
+            <div className='flex items-center gap-2 text-3xl font-extrabold'>
+              <img
+                src='./flame.png'
+                className='w-5'
+              />
+              UTILITIES SAVE
+            </div>
+            <div className=''>
+              Here at utilities save. our aim is to provide you with maximum
+              savings and best services for your business utilities. we search
+              across most of the uk suppliers both small and large and identify
+              what is the best possible deal for your business.
+            </div>
           </div>
-          <div className='font-medium'>
-            Copyright © 2016-2023 utilitiessave.com, All Right Reserved This
-            Website
-          </div>
-        </div>
-        <div className='flex flex-col items-start w-max'>
-          <div className='text-lg font-bold'>Find Us On Google</div>
-          <ul>
-            <li>Best Energy Prices UK</li>
-            <li>Best Energy Prices UK</li>
-            <li>Best Energy Prices UK</li>
-            <li>Best Energy Prices UK</li>
-            <li>Best Energy Prices UK</li>
-          </ul>
-        </div>
-        <div className='flex flex-col items-start gap-2 w-max'>
-          <div className='flex flex-col'>
-            <div className='text-lg font-bold'>Contact Us</div>
-            <ul>
+          <div className='flex flex-col items-start gap-4 w-max'>
+            <div className='flex flex-col items-start gap-4 w-max'>
+              <div className='ml-1 text-xl font-bold'>Help</div>
+              <ul className='flex flex-col gap-2'>
+                <li>
+                  <Link className='flex items-center gap-2 font-medium transition-all group hover:text-primaryGreen'>
+                    <FiChevronRight className='text-xl transition-all text-primaryGreen group-hover:translate-x-1' />{' '}
+                    Complaints Procedure
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div className='ml-1 text-xl font-bold'>Find Us On Google</div>
+            <ul className='flex flex-col gap-2'>
               <li>
-                <b>Post: </b>37 Elmbourne Road London, SW17 8JS.
+                <Link className='flex items-center gap-2 font-medium transition-all group hover:text-primaryGreen'>
+                  <FiChevronRight className='text-xl transition-all text-primaryGreen group-hover:translate-x-1' />{' '}
+                  Best Best Energy Prices UK
+                </Link>
               </li>
               <li>
-                <b>Email: </b>info@utilitiessave.com
+                <Link className='flex items-center gap-2 font-medium transition-all group hover:text-primaryGreen'>
+                  <FiChevronRight className='text-xl transition-all text-primaryGreen group-hover:translate-x-1' />{' '}
+                  Best Cheap Energy Prices in UK
+                </Link>
               </li>
               <li>
-                <b>Phone: </b> 0207 1112 9116
+                <Link className='flex items-center gap-2 font-medium transition-all group hover:text-primaryGreen'>
+                  <FiChevronRight className='text-xl transition-all text-primaryGreen group-hover:translate-x-1' />{' '}
+                  Best Cheap Electricity in UK
+                </Link>
+              </li>
+              <li>
+                <Link className='flex items-center gap-2 font-medium transition-all group hover:text-primaryGreen'>
+                  <FiChevronRight className='text-xl transition-all text-primaryGreen group-hover:translate-x-1' />{' '}
+                  Best Electricity Comparison in UK
+                </Link>
+              </li>
+              <li>
+                <Link className='flex items-center gap-2 font-medium transition-all group hover:text-primaryGreen'>
+                  <FiChevronRight className='text-xl transition-all text-primaryGreen group-hover:translate-x-1' />{' '}
+                  Best Best Energy Prices in UK
+                </Link>
               </li>
             </ul>
           </div>
-          <div className='flex flex-col'>
-            <div className='text-lg font-bold'>Help</div>
-            <ul>
-              <li>Complaints Procedure</li>
-            </ul>
+          <div className='flex flex-col items-start gap-2 w-max'>
+            <div className='flex flex-col'>
+              <div className='text-xl font-bold'>Contact Us</div>
+              <ul>
+                <li>
+                  <b>Post: </b>37 Elmbourne Road London, SW17 8JS.
+                </li>
+                <li>
+                  <b>Email: </b>info@utilitiessave.com
+                </li>
+                <li>
+                  <b>Phone: </b> 0207 1112 9116
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <div className='px-4 py-4 font-medium text-center bg-tealGreen text-lightGreen'>
+        Copyright © 2016-2023 utilitiessave.com, All Right Reserved This Website
+      </div>
+    </>
   )
 }
 
